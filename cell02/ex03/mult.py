c53128031398c297a2e695d84fb4909f2bdcdeba
SpriteUm <107129486+SpriteUm@users.noreply.github.com>
2024-11-19 19:00:59 +0000

    succeed

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+enter_p = int(input("Enter the first number :"))
+enter_p1 = int(input("Enter the second number :"))
+result = enter_p * enter_p1
+print(f"{enter_p} x {enter_p1} = {result}")
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+elif result == 0:
+    print("The result is positive and negative.")
