68f5c85bf728eccfb03161a872591f5ec6e360aa
SpriteUm <107129486+SpriteUm@users.noreply.github.com>
2024-11-20 18:43:10 +0000

    s

--- cell03/ex01/multiplicaion_table.py
+++ cell03/ex01/multiplicaion_table.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 input_n = int(input("Enter a number\n")) 
-for i in range(input_n,10):
+for i in range(0,10):
     result =  i * input_n
     print(f"{i} x {input_n} = {result}")
