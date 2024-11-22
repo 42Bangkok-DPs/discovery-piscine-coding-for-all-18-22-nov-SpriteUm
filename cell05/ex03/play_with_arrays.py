122e309f54b0855f63d31d131ff3fbb3b721d37e
SpriteUm <107129486+SpriteUm@users.noreply.github.com>
2024-11-22 03:59:42 +0000

    a

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+ori_a = [2,8,9,48,8,22,-12,2]
+ori_n = []
+for i in ori_a:
+    if i > 5:
+        ori_n.append(i + 2)
+    else:
+        pass
+arrynew = set(ori_n)
+print(ori_a)
+print(arrynew)
